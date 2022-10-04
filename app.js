9002ee85a29aa70ca842b805cac5104955b33093
MRXAZK <mrxazk@protonmail.com>
2022-10-04 23:24:38 +0700

    Add MongoDBCON

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const blogRouter = require("./routes/BlogRoutes");
+
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api/blogs", blogRouter);
+
+//configure mongoose
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/CRUD",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Connected to MongoDB");
+    }
+  }
+);
+
+app.listen(3000, () => {
+  console.log(`Server is running on port 3000`);
+});
+
+module.exports = app;
