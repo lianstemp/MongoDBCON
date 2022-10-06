3e03c1fb2abc1aa557735920fec444b335b65f1e
MRXAZK <mrxazk@protonmail.com>
2022-10-06 20:15:43 +0700

    Edit app.js and readme

---
+++ Readme.md
@@ -0,0 +1,27 @@
+
+# MongoDBCON (Check MongoDB Connection With Application String)
+
+## Installation
+```sh
+# Install All Dependencies
+$ npm i --prefix
+```
+
+## Setup 
+
+> Create .env file in MONGODBCON root folder
+
+```sh
+MONGO_DB=YOUR_MONGO_DATABASE_NAME
+MONGO_USERNAME=YOUR_MONGO_USER
+MONGO_PASSWORD=YOUR_MONGO_PASSWORD
+MONGO_HOST=YOUR_MONGO_HOST
+MONGO_PORT=YOUR_MONGO_PORT # Mongo port default is 27017
+MONGO_CERT=YOUR_MONGO_CERT # The cert location is in ssl folder
+```
+
+## Running
+```sh
+# Start MongoDBCON on development mode
+$ npm run start
+```
--- app.js
+++ app.js
@@ -1,29 +1,53 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const fs        = require('fs')
+const path      = require('path')
+const env       = require('./env')
 const blogRouter = require("./routes/BlogRoutes");
 
 const app = express();
+
+
+const clusterUrl = `mongodb://${env.mongoose.username}:${env.mongoose.password}@${env.mongoose.host}:${env.mongoose.port || '27017'}/${env.mongoose.database}?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false`
+const ca = `${path.join(__dirname, `./ssl/${ env.mongoose.cert || 'rds-combined-ca-bundle.pem'}`)}`
+
 
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/blogs", blogRouter);
 
-//configure mongoose
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/CRUD",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
-  (err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("Connected to MongoDB");
+  //configure mongoose
+  mongoose.connect(`${clusterUrl}`, {
+      tlsCAFile: `${ca}`,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+    (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Connected to MongoDB");
+      }
     }
-  }
-);
+  );
+
+// //configure mongoose
+// mongoose.connect(
+//   //process.env.MONGODB_URI || 
+//   "mongodb://127.0.0.1:27017/blog",
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   },
+//   (err) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log("Connected to MongoDB");
+//     }
+//   }
+// );
 
 app.listen(3000, () => {
   console.log(`Server is running on port 3000`);
